3e4722127098760275d3d96848cae986159a40a0
tintzaw_HTOON <107607175+tintzawhtoon@users.noreply.github.com>
2023-01-10 22:27:43 +0630

    Final

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+// Easy Pie Chart
+$(function() {
+    $('.chart').easyPieChart({
+        size: 100,
+        barColor: "#292929",
+        scaleLength: 0,
+        lineWidth: 9,
+        trackColor: "#d2d2d2",
+        animate: 1500,
+    });
+});
